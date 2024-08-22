18bd1e977b86c2ef2485310726d55ae8295c6cab
Connor <c.meriweather55@gmail.com>
2024-08-22 15:49:42 -0500

    finished #footer, added media queries, added some JS functionality (unfinished)

---
+++ script.js
@@ -0,0 +1,28 @@
+const menu = document.querySelector("#menu");
+
+const nav = document.querySelector(".links");
+
+
+menu.onClick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+
+
+// Links to socials in FOOTER
+
+function openSocialMedia(platform) {
+    const urls = {
+        github: "https://github.com/meconnor",
+        linkedin:"linkedin.com/in/connor-meriweather-6300a2279"
+    };
+
+    const url = urls[platform];
+
+    if (url) {
+        window.open(url, "_blank");
+    } else {
+        console.error("No URL defined for the platform:", platform);
+    }
+}
