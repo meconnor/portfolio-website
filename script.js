91188c1ebba37af17ec478d3f92f16b74d5b5648
Connor <c.meriweather55@gmail.com>
2024-08-22 16:23:38 -0500

    slight edit to styles and html

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const menu = document.querySelector("#menu");
 const nav = document.querySelector(".links");
 
 
-menu.onClick = () => {
+menu.addEventListener("click", () => {
+    console.log("Menu clicked")
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
-}
+});
 
 
 
